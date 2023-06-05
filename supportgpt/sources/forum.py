c8c387c4e015b5ddaa5af2627ffa0ed393c69ba2
i1i1 <vanyarybin1@live.ru>
2023-06-05 22:52:38 +0300

    Account for paging

--- supportgpt/sources/forum.py
+++ supportgpt/sources/forum.py
@@ -8,22 +8,34 @@ class ForumSource:
         self.api_username = api_username
         self.base_url = base_url
 
-    def _fetch(self, url):
+    def _fetch(self, url, params=None):
         headers = { 'Api-Key': self.api_key, 'Api-Username': self.api_username }
-        response = requests.get(urljoin(self.base_url, url), headers=headers)
+        response = requests.get(urljoin(self.base_url, url), headers=headers, params=params)
         response.raise_for_status()
         return response.json()
 
-    def fetch_categories(self):
+    def _fetch_categories(self):
         "Returns a list of categories"
         return self._fetch('/categories.json')['category_list']['categories']
 
-    def fetch_topics(self, category_slug, category_id):
+    def _fetch_topics(self, category_slug, category_id, page=None):
         "Returns topics for a category"
-        return self._fetch(f"/c/{category_slug}/{category_id}.json")['topic_list']['topics']
+        return self._fetch(f"/c/{category_slug}/{category_id}.json", params={ "page": page })['topic_list']['topics']
 
-    def fetch_posts(self, topic_id):
+    def _fetch_posts(self, topic_id):
         "Returns posts for a topic"
         return self._fetch(f"/t/{topic_id}/posts.json")['post_stream']['posts']
+
+    def _topics(self, category_name):
+        "Returns iterator over topics in category"
+        cat = { cat['name']: cat for cat in self._fetch_categories() }[category_name]
+        page = 0
+        while True:
+            topics = self._fetch_topics(cat['slug'], cat['id'], page=page)
+            if len(topics) == 0:
+                break
+            page += 1
+            for t in topics:
+                yield t
 
 
--- tests/test_forum_source.py
+++ tests/test_forum_source.py
@@ -10,18 +10,21 @@ src = ForumSource(getenv('FORUMS_API_KEY'), getenv('FORUMS_API_USERNAME'))
 
 
 def test_categories():
-    assert src.fetch_categories() != []
-    assert "Support" in [cat['name'] for cat in src.fetch_categories()]
+    assert src._fetch_categories() != []
+    assert "Support" in [cat['name'] for cat in src._fetch_categories()]
 
 def test_topics():
-    cats = { cat['name']: cat for cat in src.fetch_categories() }
+    cats = { cat['name']: cat for cat in src._fetch_categories() }
     support = cats['Support']
-    topics = src.fetch_topics(support['slug'], support['id'])
+    topics = src._fetch_topics(support['slug'], support['id'])
     assert "I think this bug" in [top['title'] for top in topics]
 
+def test_topics_iterator():
+    assert "Error: Multi-threaded server is not available on Windows. Falling back to single thread" in [top['title'] for top in src._topics('Support')]
+
 def test_posts():
-    support  = { cat['name']: cat for cat in src.fetch_categories() }['Support']
-    topic = { top['title']: top for top in src.fetch_topics(support['slug'], support['id']) }
-    posts = src.fetch_posts(topic['I think this bug']['id'])
+    support  = { cat['name']: cat for cat in src._fetch_categories() }['Support']
+    topic = { top['title']: top for top in src._fetch_topics(support['slug'], support['id']) }
+    posts = src._fetch_posts(topic['I think this bug']['id'])
 
     assert any(post['accepted_answer'] for post in posts)
